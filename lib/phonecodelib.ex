ef74b503638a483268847a63a737eb247bd412fa
Fredrick Oluoch <menty44@gmail.com>
2021-09-27 18:03:23 +0300

    various options for retrieving data

--- lib/phonecodelib.ex
+++ lib/phonecodelib.ex
@@ -16,14 +16,32 @@ defmodule Phonecodelib do
     :world
   end
 
-  def checker(incoming) do
+  def filter_by_iso(incoming) do
     IO.puts incoming
-#    my_test =
-#      with {:ok, body} <- File.read("countries.json"),
-#         {:ok, json} <- Poison.decode(body), do: {:ok, json}
-    {a, b } = get_json
+    {_a, b } = get_json()
      b
      |> get_in( [Access.filter(&(&1["iso"] === String.upcase(incoming)))])
+  end
+
+  def filter_by_iso3(incoming) do
+    IO.puts incoming
+    {_a, b } = get_json()
+     b
+     |> get_in( [Access.filter(&(&1["iso3"] === String.upcase(incoming)))])
+  end
+
+  def filter_by_name(incoming) do
+    IO.puts incoming
+    {_a, b } = get_json()
+     b
+     |> get_in( [Access.filter(&(&1["name"] === String.upcase(incoming)))])
+  end
+
+  def filter_by_phonecode(incoming) do
+    IO.puts incoming
+    {_a, b } = get_json()
+     b
+     |> get_in( [Access.filter(&(&1["phonecode"] === incoming))])
   end
 
   def get_json() do
